36bf35b0cb719a4c79ad2bb1cbd9a5ead45771c3
wrx6666 <166891935+wrx6666@users.noreply.github.com>
2025-02-04 18:29:13 +0800

    Update db.php

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 $host = 'localhost';
 $dbname = 'db_test';
 $username = 'root';
-$password = 'pyaory';
+$password = '######';
 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
